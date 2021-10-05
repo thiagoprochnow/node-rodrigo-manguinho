a03822d1a124b76624a913485b3c32cd803d3b30
Thiago Henrique Prochnow <thiagoh.p@samsung.com>
2021-10-05 15:04:14 -0300

    test: ensure BCryptAdapter throws if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,12 +1,6 @@
 import bcrypt from 'bcrypt'
 import { Encrypter } from '../../data/protocols/encrypter'
 
-jest.mock('bcrypt', () => ({
-  async hash (): Promise<string> {
-    return new Promise(resolve => resolve('hash'))
-  }
-}))
-
 export class BcryptAdapter implements Encrypter {
   private readonly salt: number
 
