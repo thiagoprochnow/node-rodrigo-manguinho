877172a568f185898705d6786c1f7cd7caf24717
Thiago Prochnow <thiago@altiverse.co>
2025-06-09 20:28:35 -0300

    restart project

--- src/domain/models/account.ts
+++
@@ -1,7 +0,0 @@
-// Modelo de DB do AddAccount
-export interface AccountModel {
-  id: string
-  name: string
-  email: string
-  password: string
-}
--- src/presentation/protocols/controller.ts
+++
@@ -1,5 +0,0 @@
-import { HttpResponse, HttpRequest } from './http'
-
-export interface Controller {
-  handle (httpRequest: HttpRequest): Promise<HttpResponse>
-}
